cd7af2a62c5b04c6bf9ece893a0d961df9512005
Programer-akram <mah.akram202@gmail.com>
2022-09-30 19:11:28 +0600

    web title added

---
+++ src/Components/Exercises/Exercises.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Exercises = ({ exercise, handelAddToCart }) => {
+    const { time, picture, name, about } = exercise;
+
+    return (
+        <div className="card w-64 bg-base-100 shadow-xl">
+            <figure className="pt-5">
+                <img src={picture} alt="Shoes" className="rounded-xl card-img" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>{about ? about.slice(0, 100) : 'Nothing'}</p>
+                <h3 className="text-lg font-semibold">
+                    Time required : {time}s
+                </h3>
+                
+            </div>
+
+
+            
+        </div>
+    );
+};
+
+export default Exercises;
+
+
